d726da99f658c37dcac60efba984a32060bb4656
ngochoai0810 <ngochoai0h1a@gmail.com>
2024-07-24 20:02:48 +0700

    LPL commit

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,5 @@
 const { pool } = require("../db");
-const bcrypt = require("bcrypt");
+const bcrypt = require('bcrypt');
 const jwt = require("jsonwebtoken");
 const mailService = require("../db").mailler;
 const crypto = require("crypto");
