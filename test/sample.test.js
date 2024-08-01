630c9bdb90c913b37cd9c1176e720a8904ede627
ngochoai0810 <156788607+ngochoai0810@users.noreply.github.com>
2024-08-01 20:18:06 +0700

    Create sample.test.js

---
+++ test/sample.test.js
@@ -0,0 +1,12 @@
+const { expect } = require("chai");
+
+describe("Sample Test", function () {
+  it("should return true", function () {
+    expect(true).to.be.true;
+  });
+
+  it("should return false", function () {
+    expect(false).to.be.false;
+  });
+});
+
